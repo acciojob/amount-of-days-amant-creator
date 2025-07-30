609802159a19c22b036974269230ddb022becf25
amant-creator <at0068812@gmail.com>
2025-07-30 20:37:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfYear(dm) {
+	if((dm % 4 ===0 && dm % 100 !== 0) || (dm % 400 === 0)){
+
+		return 366;
+	}
+	else{
+	return 365;
+	}
+	
+}
+console.log(daysOfYear(2024))
